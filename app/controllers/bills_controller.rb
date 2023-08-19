167445038cfb743661e663abf4882f0a262ec4be
Kola Kolade <kolakolade@gmail.com>
2023-08-19 12:53:08 +0100

    Add helper methods to bill controller and app/helper/bill for business logic

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -71,4 +71,17 @@ class BillsController < ApplicationController
     def bill_params
       params.require(:bill).permit(:name, :icon, :author_id)
     end
+
+    def calculate_item_count(bill)
+      Item.where(bill_id: bill.id).count
+    end
+  
+    def calculate_item_amount(bill)
+      @items = Item.where(bill_id: bill.id)
+      item_amount = 0
+      @items.each do |item|
+        item_amount += item.amount
+      end
+      item_amount
+    end
 end
